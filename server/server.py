003a5525fa99494eb2b3d489066f43b92f22d7f7
Karim <ksadek@gmail.com>
2026-03-02 09:49:17 +0100

    Configure server for Railway deployment with dynamic port

--- server/server.py
+++ server/server.py
@@ -225,7 +225,9 @@ def get_stocks():
         return jsonify(stocks_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 @app.route('/api/news/<ticker>', methods=['GET'])
 def get_news(ticker):
